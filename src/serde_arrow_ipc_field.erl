995c7cb6596a8586a72fd00139ad26619a0f3ed5
Benjamin Philip <benjamin.philip495@gmail.com>
2023-06-01 22:48:18 +0530

    Add docs

--- src/serde_arrow_ipc_field.erl
+++ src/serde_arrow_ipc_field.erl
@@ -1,12 +1,51 @@
+%% @doc Provides a record and functions to deal with individual fields in
+%% a Schema
+%%
+%% A Field[1] represents a single column in a table (which is represented by a
+%% Schema[2]). This module provides a record and a function to manage all the
+%% metadata required to represent a column. Metadata such as:
+%%
+%% <ol>
+%%  <li>
+%%      `name': The Name of the column. This can either be a string or null.
+%%  </li>
+%%  <li>
+%%      `nullable': A boolean representing whether a column can have null values.
+%%      Generally true.
+%%  </li>
+%%  <li>
+%%      `type': The Layout of the column
+%%  </li>
+%%  <li>
+%%      `dictionary': A boolean representing if the column is dictionary encoded
+%%  </li>
+%%  <li>
+%%      `children': The child fields of a nested datatype
+%%  </li>
+%%  <li>
+%%      `custom_metadata': A list of custom metadata in key-value format
+%%  </li>
+%% </ol>
+%%
+%% Currently, dictionary encoding and custom metadata are not supported, but
+%% they have been added for forwards comapatibility.
+%%
+%% [1]: [https://github.com/apache/arrow/blob/3456131ab7350bee5d9569ffd63d3f0ee713991c/format/Schema.fbs#L469-L492]
+%%
+%% [2]: [https://github.com/apache/arrow/blob/3456131ab7350bee5d9569ffd63d3f0ee713991c/format/Schema.fbs#L514-L530]
+%% @end
 -module(serde_arrow_ipc_field).
 -export([from_erlang/1, from_erlang/2]).
 
 -include("serde_arrow_ipc_schema.hrl").
 
+%% @doc Creates a field given the type of the column. Assigns the name as
+%% `undefined'.
 -spec from_erlang(Type :: serde_arrow_type:arrow_longhand_type()) -> Field :: #field{}.
 from_erlang(Type) ->
     from_erlang(Type, undefined).
 
+%% @doc Creates a field given the type and name of the column.
 -spec from_erlang(Type :: serdelayoutow_type:arrow_longhand_type(), Name :: string() | undefined) ->
     Field :: #field{}.
 from_erlang(Type, Name) when tuple_size(Type) =:= 2 ->
--- src/serde_arrow_ipc_schema.erl
+++ src/serde_arrow_ipc_schema.erl
@@ -1,3 +1,40 @@
+%% @doc Provides a record and functions to deal with Schemas
+%%
+%% A Schema[1] represents a table, or a list of arrays of equal length. This
+%% module provides a record and a function to manage all the metadata required
+%% to represent a schema. Metadata such as:
+%%
+%% <ol>
+%%  <li>
+%%      `endianness': The Endianness of the table. One of `little' or `big'.
+%%      Defaults to `little'.
+%%  </li>
+%%  <li>
+%%      `fields': The list of fields[2] in a table.
+%%  </li>
+%%  <li>
+%%      `type': The Layout of the column
+%%  </li>
+%%  <li>
+%%      `custom_metadata': A list of custom metadata in key-value format
+%%  </li>
+%%  <li>
+%%      `features': Any features used by the table which may not be present in
+%%      other implementations of Arrow.
+%%  </li>
+%% </ol>
+%%
+%% Currently, big endianness, custom metadata and features are not supported,
+%% but they have been added for forwards comapatibility.
+%%
+%% You can find Schemas in the Arrow spec here[3].
+%%
+%% [1]: [https://github.com/apache/arrow/blob/3456131ab7350bee5d9569ffd63d3f0ee713991c/format/Schema.fbs#L514-L530]
+%%
+%% [2]: [https://github.com/apache/arrow/blob/3456131ab7350bee5d9569ffd63d3f0ee713991c/format/Schema.fbs#L469-L492]
+%%
+%% [3]: [https://arrow.apache.org/docs/format/Columnar.html#schema-message]
+%% @end
 -module(serde_arrow_ipc_schema).
 -export([from_erlang/1]).
 -export_type([endianness/0, feature/0]).
@@ -5,14 +42,14 @@
 -include("serde_arrow_ipc_schema.hrl").
 
 -type endianness() :: little | big.
-%% Endianness of the data
+%% Endianness of the data. Either `little' or `big'.
 
 -type feature() :: unused | dictionary_replacement | compressed_body.
 %% Features used in the data which may not be present in other implementations.
 %% See the definition:
-%%
-%% https://github.com/apache/arrow/blob/3456131ab7350bee5d9569ffd63d3f0ee713991c/format/Schema.fbs#L51-L78
+%% [https://github.com/apache/arrow/blob/3456131ab7350bee5d9569ffd63d3f0ee713991c/format/Schema.fbs#L51-L78]
 
+%% @doc Creates a Schema given an ordered list of fields.
 -spec from_erlang(Fields :: [#field{}]) -> Schema :: #schema{}.
 from_erlang(Fields) ->
     #schema{fields = Fields}.
