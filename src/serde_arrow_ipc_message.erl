7b1f8611e9cd571d610bb6ff2ef70a3050fd05bc
Benjamin Philip <benjamin.philip495@gmail.com>
2023-06-09 00:43:21 +0530

    Add docs

--- src/serde_arrow_ipc_message.erl
+++ src/serde_arrow_ipc_message.erl
@@ -28,9 +28,16 @@
 %% Currently, changing the version and custom metadata are not supported, but
 %% they have been added for forwards comapatibility.
 %%
+%% This module also provides the `to_ipc/1' function which serializes the
+%% message into the Encapsulated Message Format[3]. However, this function gives
+%% <em>incomplete output</em> with invalid metadata, because of an unsatisfied
+%% dependency on flatbuffers, which is required for serializing the metadata.
+%%
 %% [1]: [https://arrow.apache.org/docs/format/Columnar.html#schema-message]
 %%
 %% [2]: [https://arrow.apache.org/docs/format/Columnar.html#recordbatch-message]
+%%
+%% [3]: [https://arrow.apache.org/docs/format/Columnar.html#encapsulated-message-format]
 %% @end
 -module(serde_arrow_ipc_message).
 -export([from_erlang/1, from_erlang/2, to_ipc/1]).
@@ -46,14 +53,17 @@
 %% Key-Value structure for custom metadata. See the definition for more info:
 %% [https://github.com/apache/arrow/blob/3456131ab7350bee5d9569ffd63d3f0ee713991c/format/Schema.fbs#L432-L439]
 
--spec from_erlang(Header :: #schema{}) -> Message :: #message{}.
+%% @doc Creates a message given a data header.
+-spec from_erlang(Header :: #schema{} | #record_batch{}) -> Message :: #message{}.
 from_erlang(Header) ->
     #message{header = Header, body_length = 0}.
 
--spec from_erlang(Header :: #schema{}, Body :: binary()) -> Message :: #message{}.
+%% @doc Creates a message given a data header and a body.
+-spec from_erlang(Header :: #schema{} | #record_batch{}, Body :: binary()) -> Message :: #message{}.
 from_erlang(Header, Body) ->
     #message{header = Header, body = Body, body_length = byte_size(Body)}.
 
+%% @doc Serializes a message into the Encapsulated Message Format.
 -spec to_ipc(Message :: #message{}) -> EMF :: binary().
 to_ipc(Message) ->
     %% 0xFFFFFFFF in int32
